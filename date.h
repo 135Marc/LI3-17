347a55e1b6de9c45b870eccc26a094e82c20e1d3
135Marc <ricardofp9719@hotmail.com>
2018-03-08 18:56:16 +0000

    Código da Data melhorado

--- date.h
+++ date.h
@@ -5,10 +5,12 @@
 	#include <stdio.h>
 	typedef struct dates *Date;
 
-	Date createDate(int day, int month, int year);
-	int get_day(Date d);
-	int get_month(Date d); // must return a value between 0 and 11 
-	int get_year(Date d);
-	void free_date(Date d);
+	Date nDate(int day, int month, int year);
+	void freeDate(Date d);
+	int get_Day(Date d);
+	int get_Month(Date d); // must return a value between 0 and 11 
+	int get_Year(Date d);
+	int cmpDate (Date d1, Date d2);
+	
 
 #endif
--- dates.c
+++ dates.c
@@ -30,34 +30,24 @@ int getYear (Date d) {
 	return d -> year;
 }
 
-int cmpYear (int year1,int year2) {
-	if (year1==year2) return 0;
-	return (year1>year2) ? -1 : 1;
-}
-
-int cmpMonth (int Month1, int Month2) {
-	if (Month1==Month2) return 0;
-	return (Month1>Month2) ? -1 : 1;
-}
-
-int cmpDay (int Day1, int Day2) {
-	if (Day1==Day2) return 0;
-	return (Day1>Day2) ? -1 : 1;
+int cmpVal (int a,int b) {
+	if (a==b) return 0;
+	return (a>b) ? -1 : 1;
 }
 
 int cmpDate (Date d1, Date d2) {
-	if (cmpYear(d1->year,d2->year)==1) return 1;
-		else if (cmpYear(d1->year,d2->year)==-1) return -1;
-	if (cmpYear(d1->year,d2->year) == 0 && cmpMonth(d1->month,d2->month) == 1) return 1;
-		else if (cmpYear(d1->year,d2->year) == 0 && cmpMonth(d1->month,d2->month) == -1) return -1;
-	if (cmpYear(d1->year,d2->year) == 0 && cmpMonth(d1->month,d2->month) == 0 && cmpDay(d1->day,d2->day)==1) return 1;
-		else if (cmpYear(d1->year,d2->year) == 0 && cmpMonth(d1->month,d2->month) == 0 && cmpDay(d1->day,d2->day)) return -1;	
+	if (cmpVal(d1->year,d2->year)==1) return 1;
+		else if (cmpVal(d1->year,d2->year)==-1) return -1;
+	if (cmpVal(d1->year,d2->year) == 0 && cmpVal(d1->month,d2->month) == 1) return 1;
+		else if (cmpVal(d1->year,d2->year) == 0 && cmpVal(d1->month,d2->month) == -1) return -1;
+	if (cmpVal(d1->year,d2->year) == 0 && cmpVal(d1->month,d2->month) == 0 && cmpVal(d1->day,d2->day)==1) return 1;
+		else if (cmpVal(d1->year,d2->year) == 0 && cmpVal(d1->month,d2->month) == 0 && cmpVal(d1->day,d2->day)) return -1;	
 	return 0;
 }
 
 int main () {
-	Date d1 = nDate(4,4,2017);
-	Date d2 = nDate(5,4,2017);
+	Date d1 = nDate(6,3,2018);
+	Date d2 = nDate(7,3,2018);
 	printf("%d\n",cmpDate(d1,d2));
 	return 0;
 }
